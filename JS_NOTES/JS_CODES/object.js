9e8a585cf79274c0209335f91d144e0a409602bb
vinicius-jesus <vjbfvinicius@hotmail.com>
2024-05-22 17:32:52 -0300

    initial commit

---
+++ JS_NOTES/JS_CODES/object.js
@@ -0,0 +1,23 @@
+
+
+const pedido = () => {
+    return 
+}
+
+const hamburger = {
+    nome : "big-mac",
+    pao : "brioche",
+    alface : 1,
+    queijo : 1,
+    carne : 2,
+    tomate : 0,
+    molho: true,
+}
+
+let {nome, queijo} = hamburger;
+
+nome = "quateirao"
+queijo = 2
+
+console.log(nome, queijo)
+console.log(hamburger.nome, hamburger.queijo)
